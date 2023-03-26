25317abf261266175db9a2290bcb3fdbda8e7f94
abc123abcAAAA <2014106247@qq.com>
2023-03-27 01:32:08 +0800

    auth

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const App = () => {
     <div style={{height:'100vh'}}>
       {/* Suspense组件需要fallback传入一个组件或者HTML标签来进行懒加载效果的应急 */}
       <Suspense fallback={<Loading />}>
-        {element} {/* 展示区 */}
+        {/* 展示区 */}
+        {element}
       </Suspense>
     </div>
   )
